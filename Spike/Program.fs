9977d10ed504913cafb492bfd7f065d1e351a50d
Moujib Aghrout <moujib.aghrout@codecomputerlove.com>
2018-04-30 11:13:42 +0100

    feat: full spike

--- Spike/Program.fs
+++ Spike/Program.fs
@@ -1,8 +1,28 @@
-﻿// Learn more about F# at http://fsharp.org
+﻿open canopy.runner.classic
+open canopy.configuration
+open canopy.classic
 
-open System
+chromeDir <- "bin\\Debug\\netcoreapp2.0"
+start chrome
 
-[<EntryPoint>]
-let main argv =
-    printfn "Hello World from F#!"
-    0 // return an integer exit code
+resize (1920, 1080)
+
+context ("Plumbs test run")
+"Search and check it's been searched" &&& fun _ ->
+    url "https://www.plumbs.co.uk/"
+    click "#search_toggle"
+    "#search_drop_down" << "covers"
+    press enter
+    "h1.red" == "Search"
+
+"Check if title is correct" &&& fun _ ->
+    let thisTitle = title().Trim()
+    thisTitle === "Search | Plumbs"
+
+"Checked if main content area is shown" &&& fun _ ->
+    click ".search-new li a"
+    displayed ".main_content_area"
+
+run()
+
+quit()
